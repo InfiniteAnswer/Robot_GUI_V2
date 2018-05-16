6e2039f378b51b2ed001d87ab58175c8040668fd
InfiniteAnswer <sampervictor@gmail.com>
2018-05-16 08:49:18 +0200

    Added runtime state object

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ import Manual
 import Settings
 import TilePrint
 import Controls
+import RuntimeState
 
 
 root = tk.Tk()
 root.withdraw()
+state = RuntmeState.RuntimeState()
 
 # Setup main window
 background = tk.Toplevel(root, background='black', width=640, height=480)
@@ -23,12 +25,12 @@ background.title("PxlRT Studio")
 background.geometry("640x480")
 
 # Create 4 windows that can be displayed in the info panel
-initialise_panel = Initialise.Initialise(background)
-manual_panel = Manual.Manual(background)
-settings_panel = Settings.Settings(background)
-tileprint_panel = TilePrint.TilePrint(background)
+initialise_panel = Initialise.Initialise(background, state)
+manual_panel = Manual.Manual(background, state)
+settings_panel = Settings.Settings(background, state)
+tileprint_panel = TilePrint.TilePrint(background, state)
 
 # Create main controls
-controls = Controls.Controls(background, initialise_panel, manual_panel, settings_panel, tileprint_panel)
+controls = Controls.Controls(background, initialise_panel, manual_panel, settings_panel, tileprint_panel, state)
 
 root.mainloop()
